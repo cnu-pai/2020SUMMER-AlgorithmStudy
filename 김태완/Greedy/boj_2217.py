ac008de4570d0e7238667e97ec9d5b7e03bea26a
Twan2da <ktww88@gmail.com>
2020-07-06 13:44:15 +0900

    로프

---
+++ 김태완/Greedy/boj_2217.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/2217
+# User ID : thornsal
+# Greedy
+
+rope_number = int(input())
+rope_capability = []
+weight = 0
+
+for _ in range(rope_number):
+    rope_capability.append(int(input()))
+
+rope_capability.sort(reverse = True)
+
+for i in range(rope_number):
+    new_weight = rope_capability[i] * (i+1)
+    if new_weight > weight:
+        weight = new_weight
+
+print(weight)
+

09cc8319e91cc9dd7a34c57e3b48de12e0cc865d
Twan2da <ktww88@gmail.com>
2020-07-07 18:00:58 +0900

    ATM

---
+++ 김태완/boj_11399.py
@@ -0,0 +1,13 @@
+n = int(input())
+times = list(map(int,input().split(' ')))
+times.sort()
+
+wait_time = 0
+# times[0] + ( times[0] + times[1] ) +  .. + ( times[0] + times[1] + times[2] + times[3] + times[4] )
+
+for i in range(n):
+    wait_time += times[i] * (n-i)
+
+print(wait_time)
+
+

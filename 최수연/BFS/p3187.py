092c393ccf04bb5ca1dbd3f92b4dface62137337
SuyeonChoi <tndus970120@gmail.com>
2020-07-12 17:29:39 +0900

    Update 양치기 꿍variables

--- 최수연/BFS/p3187.py
+++ 최수연/BFS/p3187.py
@@ -1,21 +1,21 @@
 from collections import deque
 R, C = map(int, input().split())
 queue = deque()
-sheep = 0
-wolf = 0
+totalSheep = 0
+totalWolf = 0
 visited = [[0]*C for _ in range(R)]
 fence = [list(input()) for _ in range(R)]
 for i in range(R):
     for j in range(C):
         if fence[i][j] == 'v':
-            wolf += 1
+            totalWolf += 1
         elif fence[i][j] == 'k':
-            sheep += 1
+            totalSheep += 1
         if fence[i][j] == 'v' and visited[i][j] == 0:
             queue.append((i, j))
             visited[i][j] = 1
-            wolf_num = 1
-            sheep_num = 0
+            countWolf = 1
+            countSheep = 0
             while queue:
                 x, y = queue.popleft()
                 for dx, dy in (-1, 0), (1, 0), (0, -1), (0, 1):
@@ -30,22 +30,15 @@ for i in range(R):
                     elif fence[nx][ny] == 'k':
                         visited[nx][ny] = 1
                         queue.append((nx, ny))
-                        sheep_num += 1
+                        countSheep += 1
                     elif fence[nx][ny] == 'v':
                         visited[nx][ny] = 1
                         queue.append((nx, ny))
-                        wolf_num += 1
-            if sheep_num > wolf_num:
-                wolf = wolf - wolf_num
+                        countWolf += 1
+            if countSheep > countWolf:
+                totalWolf = totalWolf - countWolf
             else:
-                sheep = sheep - sheep_num
+                totalSheep = totalSheep - countSheep
 
-
-
-
-
-
-
-
-print(sheep, wolf)
+print(totalSheep, totalWolf)
 

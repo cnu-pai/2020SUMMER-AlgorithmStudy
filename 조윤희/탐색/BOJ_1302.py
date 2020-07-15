8cb3088d7d2a38d61ae45656fe6be4c4ada8af6d
Choyunhui <a01022371341@gmail.com>
2020-07-15 18:28:57 +0900

    Update BOJ_1302.py

--- 조윤희/탐색/BOJ_1302.py
+++ 조윤희/탐색/BOJ_1302.py
@@ -1,35 +1,21 @@
+arr =[]
+val = []
+for i in range(int(input())):
+    title = input()
+    try: #제목 있을때
+        val[arr.index(title)] += 1
+    except: #제목이 없을때
+        arr.append(title)
+        val.append(1)
 
-#sys.stdin=open('input.txt',"r")
-input=sys.stdin.readline
-n,m,v=map(int,input().split())
+max_val = max(val)
+max_arr = arr[val.index(max_val)]
 
-matrix=[[0]*(n+1) for _ in range(n+1)]
-
-for _ in range(m):
-    adj=list(map(int,input().split()))
-    matrix[adj[0]][adj[1]]=1
-    matrix[adj[1]][adj[0]]=1
-
-def dfs(point,row, visited):
-    visited+=[point]
-    for search in range(len(row[point])):
-        if row[point][search] and search not in visited:
-
-  return visited
-
-def bfs(point,row,visited):
-    queue=[point]
-    visited=[point]
-    while queue:
-        point=queue.pop(0)
-
-
-
-
-
-
-
-
-
-
-
+for i in range(len(arr)): #팔린 권수 비교
+    try: #최대 팔린 권수와 동일한 경우 있을때
+        val[val.index(max_val)] = 0
+        if max_arr>arr[val.index(max_val)]:
+            max_arr = arr[val.index(max_val)]
+    except Exception as e: #없으면 종료
+        break
+print(max_arr)

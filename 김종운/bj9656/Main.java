5769bff8db07c2f9ae91a84cd3cf221e631116aa
JongunKim <potionkr@gmail.com>
2020-07-25 22:43:43 +0900

    Add 9656 solution

---
+++ 김종운/bj9656/Main.java
@@ -0,0 +1,13 @@
+package bj9656;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase= Integer.parseInt(br.readLine());
+        System.out.println(testCase%2==0?"SK":"CY");
+    }
+}

73541110484ba5a5131c4f36ad6e9b6633c19648
Yami <31057849+siontama@users.noreply.github.com>
2020-07-27 09:16:59 +0900

    공유기 설치

---
+++ 강시온/2110.cc
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool possible(vector<int> &a, int c, int x) {
+	int cnt = 1;
+	int last = a[0];
+	for (int house : a) {
+		if (house - last >= x) {
+			cnt += 1;
+			last = house;
+		}
+	}
+	return cnt >= c;
+}
+
+int main()
+{
+	int n, c;
+	cin >> n >> c;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a.begin(), a.end());
+
+	int l = 1;
+	int r = a[n - 1] - a[0];
+
+	int ans = 1;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+		if (possible(a, c, mid)) {
+			if (ans < mid) {
+				ans = mid;
+			}
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+
+
+	cout << ans << "\n";
+}

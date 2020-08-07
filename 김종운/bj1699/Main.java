3618349cd5ef05115be9790e23aefd7e2768b8c4
potionk <potionkr@gmail.com>
2020-08-07 15:57:19 +0900

    Add 1699 solution

---
+++ 김종운/bj1699/Main.java
@@ -0,0 +1,22 @@
+package bj1699;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}

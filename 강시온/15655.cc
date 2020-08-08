c306a07ecad15e3c0c3887af0b1a5afaa4b1d265
Yami <31057849+siontama@users.noreply.github.com>
2020-08-08 11:38:47 +0900

    N과 M (6)

---
+++ 강시온/15655.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int selec[10];
+vector<int> input;
+vector<int> V;
+int n, m;
+
+void dfs(int idx, int cnt)
+{
+	if (cnt == m) {
+		for (int i = 0; i < V.size(); i++) {
+			cout << V[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		if (selec[i] == true)continue;
+		selec[i] = true;
+		V.push_back(input[i]);
+		dfs(i + 1, cnt + 1);
+		V.pop_back();
+		selec[i] = false;
+	}
+
+}
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		input.push_back(x);
+	}
+	sort(input.begin(), input.end());
+	dfs(0, 0);
+}

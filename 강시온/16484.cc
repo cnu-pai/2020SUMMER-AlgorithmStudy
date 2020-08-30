904f33c1e869c5d267810da1460ca8851b133304
Yami <31057849+siontama@users.noreply.github.com>
2020-08-30 21:25:04 +0900

    작도하자! - ①

---
+++ 강시온/16484.cc
@@ -0,0 +1,24 @@
+#define _USE_MATH_DEFINES
+
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+
+#include<algorithm>
+#include<functional>
+
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+
+using namespace std;
+const int INF = numeric_limits<int>::max();
+const double pi = M_PI;
+
+int main()
+{
+	double n, d;
+	cin >> n >> d;
+	printf("%.1lf", n / 2 - d);
+}
